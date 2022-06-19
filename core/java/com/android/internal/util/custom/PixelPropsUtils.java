a87a58f3731b9872a10cd3cb9fc487284a43c9ec
Ivan Martynov <johnivan19999@gmail.com>
2022-06-19 09:53:57 +0530

    base: pixelproputils: remove pubg newstate and fortnite from gameprops

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -72,7 +72,6 @@ public class PixelPropsUtils {
             "com.tencent.ig",
             "com.pubg.imobile",
             "com.pubg.krmobile",
-            "com.pubg.newstate",
             "com.vng.pubgmobile",
             "com.rekoo.pubgm",
             "com.tencent.tmgp.pubgmhd",
@@ -80,7 +79,6 @@ public class PixelPropsUtils {
             "com.riotgames.league.wildrifttw",
             "com.riotgames.league.wildriftvn",
             "com.netease.lztgglobal",
-            "com.epicgames.fortnite",
             "com.epicgames.portal"
     };
 
